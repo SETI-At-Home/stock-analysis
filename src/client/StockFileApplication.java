90991f3c1d168214fb936310fc65b83d629cb9e8
Vladimir Bozhinovski <bozhinovski.vladimir@gmail.com>
2017-12-12 15:05:25 +0100

    populateStockFileData method finished

--- src/client/StockFileApplication.java
+++ src/client/StockFileApplication.java
@@ -10,7 +10,7 @@ import fileprocessors.StockFileReader;
 
 public class StockFileApplication {
 
-    public static void main(String args[]) throws IOException{
+    public static void main(String args[]) throws IOException {
         StockFileReader fr = new StockFileReader("table.csv");
 
         List<HashMap<String, Double>> dataResult = populateStockFileData(fr.getHeaders(), fr.readFileData());
@@ -19,16 +19,20 @@ public class StockFileApplication {
         fileData.printData();
         System.out.println(dataResult.size());
     }
-    /**
-     * Complete the method body so that it returns the given structure needed to
-     * populate the data field in the StockFileData class.
-     * @param headers
-     * @param lines
-     * @return List
-     */
-    public static List<HashMap<String, Double>> populateStockFileData(List<String> headers, List<String> lines){
-        List<HashMap<String, Double>> dataResult = new ArrayList<>();
 
+    public static List<HashMap<String, Double>> populateStockFileData(List<String> headers, List<String> lines) {
+        List<HashMap<String, Double>> dataResult = new ArrayList<>();
+        for (String line : lines) {
+            String[] values = line.split(",");
+            int counter = 0;
+            HashMap<String, Double> headerValueMap = new HashMap<>();
+            for (String value : values) {
+                double dval = Double.parseDouble(value);
+                headerValueMap.put(headers.get(counter), dval);
+                counter++;
+            }
+            dataResult.add(headerValueMap);
+        }
         return dataResult;
     }
 
